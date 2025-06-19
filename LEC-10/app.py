3405ea292b86edb0c02df1f08746df6cc9887855
mahmoud26796 <mahmoudmrm96@gmail.com>
2025-06-19 21:25:08 +0300

    finished the lecture's code

--- LEC-10/app.py
+++ LEC-10/app.py
@@ -19,5 +19,28 @@ def count_words(s):
     new_s = s.split(' ')
     return len(new_s)
 
-print(count_words("Mahmoud Loves Sara Hany"))
-print(count_words("Helo it's Me"))
+# print(count_words("Helo it's Me"))
+
+"""
+ write a function that does the same as the function above it (count_words) 
+ but return a list with the words sorted in aplhabet order
+"""
+def sort_words(sen):
+     list_of_sen = sorted(sen.split(' '))
+    #  lecture's first approach
+    #  list_of_sen = sen.split(' ')
+    #  list_of_sen.sort() 
+     return list_of_sen 
+
+# print(sort_words("Look At The Photograph"))
+
+
+"""
+ Let's Write A Function That Mutate The Inputs
+"""
+def squares(L):
+    for i, e in enumerate(L):
+         L[i] = e**2
+    return L
+
+print(squares([2, 3, 4]))
